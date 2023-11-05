8e260f3cbe6dd6a6ad2c686f8ca4c3f21c1056c3
Alejandro <149948728+yuukidadog@users.noreply.github.com>
2023-11-05 22:19:10 +0800

    Create learning for fun.cpp

---
+++ learning for fun.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main () {
+	using namespace std;
+
+	int age;
+	
+	cout << "how old are you?" << endl;
+	cin >> age;
+	cout << " you are " << age << " years old" << endl;
+	
+	
+	system ("pause");
+	return 0;
+}
